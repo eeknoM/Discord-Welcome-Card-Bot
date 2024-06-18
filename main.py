4107877abc0e70086d57344c9d4845aa627fa40a
eeknoM <159506915+eeknoM@users.noreply.github.com>
2024-06-19 09:40:32 +1000

    code comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import json
 import welcomer
 
+#load JSON file
 with open('config.json') as config_file:
     config = json.load(config_file)
 
@@ -11,6 +12,7 @@ intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+#setup welcomer module
 welcomer.setup_welcomer(bot)
 
 bot.run(config['bot_token'])
